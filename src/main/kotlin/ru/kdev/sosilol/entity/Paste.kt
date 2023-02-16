f38cbb89674010535d6423fc103e14055afb40a8
fiwka <dropinclass1@gmail.com>
2023-02-16 19:19:41 +0500

    fix: code longer than 255 characters can't be added

--- src/main/kotlin/ru/kdev/sosilol/entity/Paste.kt
+++ src/main/kotlin/ru/kdev/sosilol/entity/Paste.kt
@@ -1,13 +1,12 @@
 package ru.kdev.sosilol.entity
 
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "pastes")
 open class Paste(
     @Id
     open val id: String = "",
+    @Column(columnDefinition="TEXT")
     open val code: String = "",
 )
