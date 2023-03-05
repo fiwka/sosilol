1157496bfe3471db14fc92e6f26b2e393ae8aba1
fiwka <dropinclass1@gmail.com>
2023-03-05 13:40:52 +0500

    ci: fix docker push x2

--- src/main/kotlin/ru/kdev/sosilol/main.kt
+++ src/main/kotlin/ru/kdev/sosilol/main.kt
@@ -3,11 +3,15 @@ package ru.kdev.sosilol
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import kotlin.system.exitProcess
 
 @EnableCaching
 @SpringBootApplication
 class SosiLolApplication
 
 fun main(args: Array<String>) {
+    if (System.getenv("CI") != null)
+        exitProcess(0)
+
     runApplication<SosiLolApplication>(*args)
 }
